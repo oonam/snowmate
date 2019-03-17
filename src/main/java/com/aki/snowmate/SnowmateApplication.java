324aef292a863b412748bc4db4f52baa17f78e41
Áki <akos.makovics@gmail.com>
2019-03-17 12:55:12 +0100

    first

---
+++ src/main/java/com/aki/snowmate/SnowmateApplication.java
@@ -0,0 +1,13 @@
+package com.aki.snowmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowmateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SnowmateApplication.class, args);
+  }
+
+}
